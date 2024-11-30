eb9ef6a647a54c6dcab988c231e7e63a3af73b6f
Mohamed Wael <127055244+more2012@users.noreply.github.com>
2024-11-30 20:57:58 +0200

    Update Arrival of the General.cpp

--- Rookies/Task1/Arrival of the General.cpp
+++ Rookies/Task1/Arrival of the General.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> heights(n);
+    for (int i = 0; i < n; i++) {
+        cin >> heights[i];
+    }
+
+    int max_height = heights[0], min_height = heights[0];
+    int max_pos = 0, min_pos = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (heights[i] > max_height) {
+            max_height = heights[i];
+            max_pos = i;
+        }
+        if (heights[i] <= min_height) {
+            min_height = heights[i];
+            min_pos = i;
+        }
+    }
+
+    int moves = max_pos + (n - 1 - min_pos);
+
+    if (max_pos > min_pos) {
+        moves -= 1;
+    }
+
+    cout << moves << endl;
+    return 0;
+}
